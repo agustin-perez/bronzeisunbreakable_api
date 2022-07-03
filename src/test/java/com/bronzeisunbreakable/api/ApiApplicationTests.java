96a391e80fe8ddb1abfca7642429f7b9a5a64f69
agustin-perez <agperez.prieto@protonmail.com>
2022-07-02 22:03:36 -0300

    Spring initializr

---
+++ src/test/java/com/bronzeisunbreakable/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bronzeisunbreakable.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
